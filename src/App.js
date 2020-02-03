38a31c7a4d9808990cea525faf1ff68d17ed7289
Ahmed Rezk <work72019@gmail.com>
2020-02-03 10:50:49 -0700

    Created theme file.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import Sidebar from "./Components/Sidebar";
 import routes from "./routes";
 import { Container } from "react-bootstrap";
 import styled, { ThemeProvider } from "styled-components";
-const theme = {
-	colors: {
-		sidebar: "#F5F5F5",
-		mainColor: "#717171",
-		warningColor: "#FFC107"
-	},
-	fonts: ["sans-serif", "Roboto"],
-	fontSizes: {
-		small: "1em",
-		medium: "2em",
-		large: "3em"
-	}
-};
+import theme from "./assets/theme";
 
 export const AppContainer = styled.div`
 	display: flex;
---
+++ src/assets/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+	colors: {
+		sidebar: "#F5F5F5",
+		mainColor: "#717171",
+		warningColor: "#FFC107"
+	},
+	fonts: ["sans-serif", "Roboto"],
+	fontSizes: {
+		small: "1em",
+		medium: "2em",
+		large: "3em"
+	}
+};
+
+export default theme;
