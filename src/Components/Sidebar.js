5e1f305517b932860c7b91eea5e04198ded674db
Ahmed Rezk <work72019@gmail.com>
2020-02-03 10:48:25 -0700

    Added Sidebar.

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
+import { Link, NavLink } from "react-router-dom";
 
 import styled from "styled-components";
+import {
+	HomeIcon,
+	AboutIcon,
+	PortfolioIcon,
+	SkillsIcon,
+	ContactIcon,
+	LinkedinIcon,
+	GithubIcon
+} from "../assets/svg";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
-export const Side = styled.div`
+const Side = styled.div`
 	background-color: ${props => props.theme.colors.sidebar};
 	min-height: 100vh;
 	width: 135px;
@@ -15,26 +24,189 @@ export const Side = styled.div`
 	border-width: 2px;
 	border-style: solid;
 	border-radius: 0px 4px 41px 16px/9px 21px 6px 0px;
+	justify-content: space-around;
+`;
+
+const Title = styled.h1`
+	margin-bottom: 2rem;
+	font-size: 3.5rem;
+	color: ${props => props.theme.colors.mainColor};
+`;
+const List = styled.ul`
+	list-style-type: none;
+	padding: 0;
+	align-items: center;
+	li {
+		display: flex;
+		justify-content: center;
+		margin-bottom: 3.8rem;
+		a {
+			display: flex;
+			width: 100%;
+			height: 100%;
+			justify-content: center;
+			padding: 0.3rem;
+			position: relative;
+		}
+		svg {
+			fill: ${props => props.theme.colors.mainColor};
+			transition-property: all;
+			transition-duration: 0.6s;
+			transition-timing-function: ease;
+		}
+		&:hover {
+			svg {
+				fill: ${props => props.theme.colors.warningColor};
+			}
+		}
+		.selectedLink {
+			&::after {
+				content: "";
+				position: absolute;
+				bottom: -7px;
+				z-index: 11111;
+				width: 80px;
+				display: flex;
+				border: 2px solid transparent;
+				border-color: ${props => props.theme.colors.warningColor};
+				border-radius: 255px 25px 225px 25px/25px 225px 25px 255px;
+			}
+			text-decoration: none;
+			svg {
+				fill: ${props => props.theme.colors.warningColor};
+			}
+		}
+	}
+`;
+const SocialMedia = styled.div`
+	display: flex;
+	align-items: center;
+	a {
+		display: flex;
+		justify-content: center;
+		margin: 0 0.5rem;
+		svg {
+			fill: ${props => props.theme.colors.mainColor};
+			transition-property: all;
+			transition-duration: 0.6s;
+			transition-timing-function: ease;
+		}
+		&:hover {
+			svg {
+				fill: ${props => props.theme.colors.warningColor};
+			}
+		}
+	}
 `;
 
 const Sidebar = props => {
 	return (
 		<Side>
-			<ul style={{ listStyleType: "none", padding: 0 }}>
+			<Title>{`<A/>`}</Title>
+			<List>
 				<li>
-					<Link to="/">Home</Link>
+					<OverlayTrigger
+						placement="right"
+						overlay={<Tooltip id="tooltip-disabled">Home</Tooltip>}
+					>
+						<NavLink exact activeClassName="selectedLink" to="/">
+							<HomeIcon />
+						</NavLink>
+					</OverlayTrigger>
 				</li>
 				<li>
-					<Link to="/bubblegum">Bubblegum</Link>
+					<OverlayTrigger
+						placement="right"
+						overlay={
+							<Tooltip id="tooltip-disabled">About Me!</Tooltip>
+						}
+					>
+						<NavLink
+							exact
+							activeClassName="selectedLink"
+							to="/about"
+						>
+							<AboutIcon />
+						</NavLink>
+					</OverlayTrigger>
 				</li>
 				<li>
-					<Link to="/shoelaces">Shoelaces</Link>
+					<OverlayTrigger
+						placement="right"
+						overlay={
+							<Tooltip id="tooltip-disabled">Portfolio</Tooltip>
+						}
+					>
+						<NavLink
+							exact
+							activeClassName="selectedLink"
+							to="/portfolio"
+						>
+							<PortfolioIcon />
+						</NavLink>
+					</OverlayTrigger>
 				</li>
-			</ul>
+				<li>
+					<OverlayTrigger
+						placement="right"
+						overlay={
+							<Tooltip id="tooltip-disabled">Skills</Tooltip>
+						}
+					>
+						<NavLink
+							exact
+							activeClassName="selectedLink"
+							to="/skills"
+						>
+							<SkillsIcon />
+						</NavLink>
+					</OverlayTrigger>
+				</li>
+				<li>
+					<OverlayTrigger
+						placement="right"
+						overlay={
+							<Tooltip id="tooltip-disabled">Contact Me</Tooltip>
+						}
+					>
+						<NavLink
+							exact
+							activeClassName="selectedLink"
+							to="/contact"
+						>
+							<ContactIcon />
+						</NavLink>
+					</OverlayTrigger>
+				</li>
+			</List>
+			<SocialMedia>
+				<OverlayTrigger
+					placement="right"
+					overlay={<Tooltip id="tooltip-disabled">Linkedin</Tooltip>}
+				>
+					<a
+						href="https://www.linkedin.com/in/ahmed-rezk-dev/"
+						target="_blank"
+						rel="noopener"
+					>
+						<LinkedinIcon />
+					</a>
+				</OverlayTrigger>
+				<OverlayTrigger
+					placement="right"
+					overlay={<Tooltip id="tooltip-disabled">Github</Tooltip>}
+				>
+					<a
+						href="https://github.com/ahmed-rezk-dev"
+						target="_blank"
+						rel="noopener"
+					>
+						<GithubIcon />
+					</a>
+				</OverlayTrigger>
+			</SocialMedia>
 		</Side>
 	);
 };
-
-Sidebar.propTypes = {};
 
 export default Sidebar;
--- src/routes.js
+++ src/routes.js
@@ -8,14 +8,24 @@ const routes = [
 		main: () => <h2>Home</h2>
 	},
 	{
-		path: "/bubblegum",
-		sidebar: () => <div>bubblegum!</div>,
-		main: () => <h2>Bubblegum</h2>
+		path: "/about",
+		sidebar: () => <div>About!</div>,
+		main: () => <h2>About</h2>
 	},
 	{
-		path: "/shoelaces",
-		sidebar: () => <div>shoelaces!</div>,
-		main: () => <h2>Shoelaces</h2>
+		path: "/portfolio",
+		sidebar: () => <div>portfolio!</div>,
+		main: () => <h2>Portfolio</h2>
+	},
+	{
+		path: "/skills",
+		sidebar: () => <div>skills!</div>,
+		main: () => <h2>Skills</h2>
+	},
+	{
+		path: "/contact",
+		sidebar: () => <div>contact!</div>,
+		main: () => <h2>Contact</h2>
 	}
 ];
 
