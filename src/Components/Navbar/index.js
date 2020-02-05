ee3fea058c749df95f6389f9baf31e07482703b2
Ahmed Rezk <work72019@gmail.com>
2020-02-04 23:13:05 -0700

    Animation - propTypes.

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,34 +1,61 @@
 import React from "react"
-
+import PropTypes from "prop-types"
 import styled from "styled-components"
-import { MenuIcon, LampIcon } from "../../assets/svg"
 import { Button } from "react-bootstrap"
+import { Spring } from "react-spring/renderprops"
+import { MenuIcon, LampIcon } from "../../assets/svg"
 
 export const Nav = styled.div`
 	display: flex;
 	min-height: 60px;
-	margin-bottom: 2rem;
+	margin-bottom: 5rem;
 	align-items: center;
 	justify-content: space-between;
 	svg {
 		fill: ${(props) => props.theme.colors.mainColor};
 	}
 `
-export default function Navbar({ setOpen, open, setThm, thm }) {
+function Navbar({ setOpen, open, setThm, thm }) {
 	return (
 		<>
 			<Nav>
-				<Button
-					variant="link"
-					onClick={() => setOpen(open === undefined ? false : !open)}
-					aria-expanded={open}
+				<Spring
+					from={{ transform: "translate3d(0,-40px,0)" }}
+					to={{ transform: "translate3d(0,0px,0)" }}
+					delay={1500}
 				>
-					<MenuIcon open={open} />
-				</Button>
-				<Button variant="link" onClick={() => setThm(!thm)}>
-					<LampIcon thm={thm} />
-				</Button>
+					{(props) => (
+						<Button
+							style={props}
+							variant="link"
+							onClick={() => setOpen(open === undefined ? false : !open)}
+							aria-expanded={open}
+						>
+							<MenuIcon open={open} />
+						</Button>
+					)}
+				</Spring>
+				<Spring
+					from={{ transform: "translate3d(0,-40px,0)" }}
+					to={{ transform: "translate3d(0,0px,0)" }}
+					delay={1500}
+				>
+					{(props) => (
+						<Button style={props} variant="link" onClick={() => setThm(!thm)}>
+							<LampIcon thm={thm} />
+						</Button>
+					)}
+				</Spring>
 			</Nav>
 		</>
 	)
 }
+
+Navbar.propTypes = {
+	setOpen: PropTypes.func.isRequired,
+	open: PropTypes.bool.isRequired,
+	setThm: PropTypes.func.isRequired,
+	thm: PropTypes.bool.isRequired
+}
+
+export default Navbar
