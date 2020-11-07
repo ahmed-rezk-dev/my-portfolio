7e8d4caa4304a60614aa0e3846670ebd395f02f2
Ahmed Rezk <ahmedrezk@Ahmeds-MacBook-Pro.local>
2020-11-07 08:46:08 -0700

    UPDATE: skills

--- src/Containers/Skills/index.js
+++ src/Containers/Skills/index.js
@@ -20,17 +20,10 @@ function Skills() {
 				</Row>
 				<Row>
 					<Col md={6} className="mt-5">
-						<CustomSpring time={700}>
+						<CustomSpring time={1100}>
 							<div className="mt-3">
-								<h4>PHP</h4>
+								<h4>JavaScript/Typescript</h4>
 								<ProgressBar now={95} />
-							</div>
-						</CustomSpring>
-
-						<CustomSpring time={900}>
-							<div className="mt-3">
-								<h4>Laravel</h4>
-								<ProgressBar now={100} />
 							</div>
 						</CustomSpring>
 
@@ -47,6 +40,21 @@ function Skills() {
 								<ProgressBar now={90} />
 							</div>
 						</CustomSpring>
+
+						<CustomSpring time={700}>
+							<div className="mt-3">
+								<h4>PHP</h4>
+								<ProgressBar now={95} />
+							</div>
+						</CustomSpring>
+
+						<CustomSpring time={900}>
+							<div className="mt-3">
+								<h4>Laravel</h4>
+								<ProgressBar now={100} />
+							</div>
+						</CustomSpring>
+
 						<CustomSpring time={1500}>
 							<div className="mt-3">
 								<h4>MySQL</h4>
@@ -60,16 +68,10 @@ function Skills() {
 							</div>
 						</CustomSpring>
 
-						<CustomSpring time={1900}>
+						<CustomSpring time={1700}>
 							<div className="mt-3">
-								<h4>Socket.io</h4>
-								<ProgressBar now={100} />
-							</div>
-						</CustomSpring>
-						<CustomSpring time={2100}>
-							<div className="mt-3">
-								<h4>Git</h4>
-								<ProgressBar now={95} />
+								<h4>AWS</h4>
+								<ProgressBar now={70} />
 							</div>
 						</CustomSpring>
 					</Col>
@@ -77,49 +79,55 @@ function Skills() {
 					<Col md={6} className="mt-5">
 						<CustomSpring time={800}>
 							<div className="mt-3">
-								<h4>HTML</h4>
+								<h4>HTML/CSS/Scss/Sass</h4>
 								<ProgressBar now={100} />
-							</div>
-						</CustomSpring>
-
-						<CustomSpring time={900}>
-							<div className="mt-3">
-								<h4>CSS</h4>
-								<ProgressBar now={95} />
 							</div>
 						</CustomSpring>
 
 						<CustomSpring time={1000}>
 							<div className="mt-3">
-								<h4>Bootstrap</h4>
-								<ProgressBar now={95} />
-							</div>
-						</CustomSpring>
-
-						<CustomSpring time={1100}>
-							<div className="mt-3">
-								<h4>JavaScript</h4>
+								<h4>Bootstrap/Material/Ant Design</h4>
 								<ProgressBar now={95} />
 							</div>
 						</CustomSpring>
 
 						<CustomSpring time={1200}>
 							<div className="mt-3">
-								<h4>React</h4>
+								<h4>React/Redux.js</h4>
 								<ProgressBar now={90} />
 							</div>
 						</CustomSpring>
 
 						<CustomSpring time={1300}>
 							<div className="mt-3">
-								<h4>Redux.js</h4>
+								<h4>Angular/Ngrx</h4>
+								<ProgressBar now={90} />
+							</div>
+						</CustomSpring>
+
+						<CustomSpring time={1900}>
+							<div className="mt-3">
+								<h4>Socket.io/Real Time</h4>
+								<ProgressBar now={100} />
+							</div>
+						</CustomSpring>
+						<CustomSpring time={2100}>
+							<div className="mt-3">
+								<h4>Git/Github/Bitbucket</h4>
+								<ProgressBar now={95} />
+							</div>
+						</CustomSpring>
+
+						<CustomSpring time={1400}>
+							<div className="mt-3">
+								<h4>Stripe payment</h4>
 								<ProgressBar now={90} />
 							</div>
 						</CustomSpring>
 
 						<CustomSpring time={1400}>
 							<div className="mt-3">
-								<h4>Google Api/Maps</h4>
+								<h4>Google Apis/Maps</h4>
 								<ProgressBar now={90} />
 							</div>
 						</CustomSpring>
