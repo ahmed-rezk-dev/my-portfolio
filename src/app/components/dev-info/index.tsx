cacd2baab7e5e57575f99381a306fd0670c3a5da
Ahmed Rezk <rezk5024@gmail.com>
2024-08-12 21:17:39 -0600

    add the reset of the props to custom components

--- src/app/components/dev-info/index.tsx
+++ src/app/components/dev-info/index.tsx
@@ -1,19 +1,20 @@
-import React, { ReactNode } from "react"
+import React, { HTMLAttributes, ReactNode } from "react"
 import styled from "styled-components"
 
-export const TextContainer = styled.div``
 export const TextCode = styled.h2`
 	color: ${(props) => props.theme.colors.success};
 `
 export const CenterElement = styled.div`
 	margin-left: 1rem;
 `
-export default function DevInfo({ element }: { element: ReactNode }) {
+
+type DevInfoType = { element: ReactNode } & HTMLAttributes<HTMLDivElement>
+export default function DevInfo({ element, ...rest }: DevInfoType) {
 	return (
-		<TextContainer>
+		<div {...rest}>
 			<TextCode>{"<h2>"}</TextCode>
 			<CenterElement>{element}</CenterElement>
 			<TextCode>{"</h2>"}</TextCode>
-		</TextContainer>
+		</div>
 	)
 }
--- src/app/components/dev-sub-info/index.tsx
+++ src/app/components/dev-sub-info/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react"
+import React, { HTMLAttributes, ReactNode } from "react"
 import styled from "styled-components"
 
 export const TextCode = styled.h5`
@@ -8,12 +8,13 @@ export const CenterElement = styled.div`
 	margin: 0 0 0 1rem;
 	font-weight: 100;
 `
-export default function DevSubInfo({ element }: { element: ReactNode }) {
+type DevSubInfoType = { element: ReactNode } & HTMLAttributes<HTMLDivElement>
+export default function DevSubInfo({ element, ...rest }: DevSubInfoType) {
 	return (
-		<>
+		<div {...rest}>
 			<TextCode>{"<p>"}</TextCode>
 			<CenterElement>{element}</CenterElement>
 			<TextCode>{"</p>"}</TextCode>
-		</>
+		</div>
 	)
 }
